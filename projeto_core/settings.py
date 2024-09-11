8d0df48d66a7145fee3b89cb304b19b50b1cf0ae
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2024-09-10 21:32:50 -0300

    5

--- projeto_core/settings.py
+++ projeto_core/settings.py
@@ -9,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=8xekpfd1+vqzcq4asf%sjy(sqymk&zzf%69=)(+kusrbs_clq'
+
+import os
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
